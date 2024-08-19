4d177977723d0d2104b0cb599cc416f2ae3da875
Daichi Hamada <153255737+Daichi-Hamada@users.noreply.github.com>
2024-08-19 09:02:50 +0900

    不具合の修正

--- script/index.js
+++ script/index.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", main)
 function main(){
     const titleElm = document.getElementById("title");
     setTimeout(()=>{
-        titleElm.setAttribute("class", "color:red;");
+        titleElm.setAttribute("style", "color:red;");
     }, 3_000);
 }
